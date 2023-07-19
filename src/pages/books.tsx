22a600bd2bb25218365b83ca6b6026955773caee
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-19 19:49:40 +0500

    Books Page

---
+++ src/pages/books.tsx
@@ -0,0 +1,8 @@
+import { withLayout } from '../layouts/layout';
+import { BooksPageComponent } from '../pageComponent';
+
+const Books = () => {
+  return <BooksPageComponent />;
+};
+
+export default withLayout(Books);
