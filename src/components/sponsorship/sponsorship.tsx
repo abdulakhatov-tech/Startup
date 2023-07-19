197a7d2423cc70b5d1f6f2650f3bf3b397446755
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-19 09:08:08 +0500

    sponsorship section

--- src/components/sponsorship/sponsorship.tsx
+++ src/components/sponsorship/sponsorship.tsx
@@ -1,5 +1,33 @@
+import { Icon } from '@chakra-ui/react';
+import Carousel from 'react-multi-carousel';
+
+import { sponsorshipCarousel } from 'src/config/carousel';
+import { trustedCompeny } from 'src/config/constants';
+import SectionTitle from '../section-title/section-title';
+
 const Sponsorship = () => {
-  return <div>Sponsorship</div>;
+  return (
+    <>
+      <SectionTitle
+        title=""
+        subtitle="Trusted by the world’s best"
+        textAlign={'center'}
+        mb={5}
+      />
+      <Carousel
+        responsive={sponsorshipCarousel}
+        arrows={false}
+        showDots={false}
+        infinite
+        autoPlay={true}
+        autoPlaySpeed={1000}
+      >
+        {trustedCompeny.map((item, idx) => (
+          <Icon key={idx} as={item} fontSize={50} />
+        ))}
+      </Carousel>
+    </>
+  );
 };
 
 export default Sponsorship;
