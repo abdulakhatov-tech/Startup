0e7b280b237ca62125d8541e10702822cfe0a993
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-20 12:38:52 +0500

    Contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,8 @@
 import { withLayout } from '../layouts/layout';
+import { ContactPageComponent } from '../pageComponent';
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return <ContactPageComponent />;
 };
 
 export default withLayout(ContactPage);
