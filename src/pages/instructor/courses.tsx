af6e1500439d916467c3a3c657f160d179a350f4
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-08-05 07:43:20 +0500

    get all courses & by slug

--- src/pages/instructor/courses.tsx
+++ src/pages/instructor/courses.tsx
@@ -1,10 +1,28 @@
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 
 import { withInstructorLayout } from '@/src/layouts/Instructor';
 import { InstructorCoursesPageComponent } from '@/src/pageComponent';
+import { InstructorService } from '@/src/services/instructor.service';
+import { CourseType } from '@/src/interfaces/course.interface';
 
-const Courses: NextPage = () => {
+const Courses: NextPage<CoursesPageType> = () => {
   return <InstructorCoursesPageComponent />;
 };
 
 export default withInstructorLayout(Courses);
+
+export const getServerSideProps: GetServerSideProps<CoursesPageType> = async ({
+  req,
+}) => {
+  const courses = await InstructorService.getAllCourses(req.cookies.refresh);
+
+  return {
+    props: {
+      courses,
+    },
+  };
+};
+
+interface CoursesPageType extends Record<string, unknown> {
+  courses: CourseType[];
+}
